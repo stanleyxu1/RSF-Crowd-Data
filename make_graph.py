58b2c5d9dea307a775e7081551700258cab887b2
stanley xu <stanleyxu@berkeley.edu>
2026-03-10 23:09:27 -0700

    increase to every 30 minute on graph

--- make_graph.py
+++ make_graph.py
@@ -11,18 +11,21 @@ df = pd.read_csv("RSF_Dataset.csv")
 df["timestamp"] = pd.to_datetime(df["timestamp"])
 
 # Extract hour
-df["hour"] = df["timestamp"].dt.hour
+df["time_bin"] = df["timestamp"].dt.round("30min")
 
 # Keep only gym open hours
+df["hour"] = df["time_bin"].dt.hour
+df["minute"] = df["time_bin"].dt.minute
 df = df[(df["hour"] >= 7) & (df["hour"] <= 23)]
 
 
 # %%
 # Group data
-grouped = df.groupby(["weekday","hour"])["percent_full"].mean().reset_index()
+grouped = df.groupby(["weekday","hour","minute"])["percent_full"].mean().reset_index()
+
 
 # Sort days and hours
-grouped = grouped.sort_values(["weekday","hour"])
+grouped = grouped.sort_values(["weekday","hour","minute"])
 
 # Map weekday names
 day_map = {
@@ -42,11 +45,15 @@ plt.figure(figsize=(10,6))
 
 for day in sorted(grouped["weekday"].unique()):
     data = grouped[grouped["weekday"] == day]
-    plt.plot(data["hour"], data["percent_full"], marker="o", label=day_map[day])
+
+    # convert hour + minute into decimal time for plotting
+    x = data["hour"] + data["minute"]/60
+
+    plt.plot(x, data["percent_full"], marker="o", label=day_map[day])
 
 plt.xlabel("Hour of Day")
 plt.ylabel("Average % Full")
-plt.title("RSF Crowd Patterns by Hour")
+plt.title("RSF Crowd Patterns by Time")
 
 plt.xticks(range(7,24))
 plt.grid(True)

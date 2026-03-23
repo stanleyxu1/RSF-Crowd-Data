0d705033a0d4344faf29803ceae874f4ece1f576
stanley xu <stanleyxu@berkeley.edu>
2026-03-23 16:49:51 -0700

    wrong variable name

--- models/Gym_Crowdedness_Models.py
+++ models/Gym_Crowdedness_Models.py
@@ -80,7 +80,7 @@ X = df.iloc[1:][features]
 y = df[1:]["percent_full"]
 
 # %%
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 # %%

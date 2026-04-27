b480f71a88cd0aaa7bd92268f0a90152466c9fe5
stanley xu <stanleyxu@berkeley.edu>
2026-04-26 21:12:22 -0700

    update

--- models/Model_Predict.py
+++ models/Model_Predict.py
@@ -1,5 +1,4 @@
 # %%
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import pandas as pd
